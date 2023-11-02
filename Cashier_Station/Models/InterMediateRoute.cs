c57f2b509f3ecce35d88d872f55133275e378cde
VMekh8 <mekhvitaliy88@gmail.com>
2023-11-02 11:27:11 +0200

    Add models for Routes

---
+++ Cashier_Station/Models/InterMediateRoute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cashier_Station.Models
+{
+    public class InterMediateRoute : Route
+    {
+        public string NameStation { get; set; }
+
+        public int  DistanceFromStart { get; set; }
+        public int DistanceFromEnd { get; set;}
+        public DateTime ArrivalTime { get; set; }
+    }
+}
---
+++ Cashier_Station/Models/Route.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cashier_Station.Models
+{
+    public class Route
+    {
+        public int IDRoute { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public string StartPoint { get; set; }
+        public string EndPoint { get; set; }
+
+        public DateTime DateofStart { get; set; }
+        public DateTime DateofEnd { get; set;}
+        public int SeatsNumber { get; set; }
+    }
+}
