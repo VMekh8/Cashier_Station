2653c0bb68df4dd9f654b1605a30f970d1e672c6
VMekh8 <mekhvitaliy88@gmail.com>
2023-11-02 11:43:42 +0200

    Add a model for Program

---
+++ Cashier_Station/Models/Cashier.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cashier_Station.Models
+{
+    public class Cashier
+    {
+        public List<Ticket> tickets = new List<Ticket>();
+        
+        public int Balance { get; set; }
+    }
+}
---
+++ Cashier_Station/Models/Transport.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cashier_Station.Models
+{
+    public class Transport
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Route IdRoute { get; set; }
+
+    }
+}
