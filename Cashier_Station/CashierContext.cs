4bb8528d8c87bd5a4bbd305f53d8b518e8f5a9c8
VMekh8 <mekhvitaliy88@gmail.com>
2023-11-11 14:48:49 +0200

    Add a Context File and build a connection string

---
+++ Cashier_Station/CashierContext.cs
@@ -0,0 +1,20 @@
+﻿using Cashier_Station.Models;
+using System;
+using System.Data.Entity;
+
+
+namespace Cashier_Station
+{
+    public class CashierContext : DbContext
+    {
+        public CashierContext() : base("DefaultConnection") {}
+
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Cashier> Cashiers { get; set; }
+        public DbSet<Route> Routes { get; set; }
+        public DbSet<InterMediateRoute> InterMediateRoutes  { get; set; }
+        public DbSet<Ticket> Tickets  { get; set; }
+        public DbSet<Transport> Transports { get; set; }
+
+    }
+}
