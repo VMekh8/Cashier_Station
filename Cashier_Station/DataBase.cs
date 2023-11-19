130ac1ec462846e952235bcb3fc1f0ec45dca455
VMekh8 <mekhvitaliy88@gmail.com>
2023-11-19 21:32:38 +0200

    delete models for entityFM , add a connection to MySQL

---
+++ Cashier_Station/DataBase.cs
@@ -0,0 +1,24 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace Cashier_Station
+{
+    public class DataBase
+    {
+        private readonly MySqlConnection connection = new MySqlConnection("server=localhost;port=3306;username=root;password=;database=cashierstation");
+
+        public void OpenConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+                connection.Open();
+        }
+        
+        public void CloseConnection()
+        {
+            if (connection.State != System.Data.ConnectionState.Open)
+                connection.Close();
+        }
+
+        public MySqlConnection GetConnection() => connection;
+
+    }
+}
