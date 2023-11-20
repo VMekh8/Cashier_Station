66a32848b027e3c57d2972e3bb3a40ea6f7f9062
VMekh8 <mekhvitaliy88@gmail.com>
2023-11-20 18:32:01 +0200

    make a logic for add ticket window

--- Cashier_Station/Admin/Ticket/AdminAddTicketWindow.cs
+++ Cashier_Station/Admin/Ticket/AdminAddTicketWindow.cs
@@ -1,4 +1,5 @@
 ﻿using Cashier_Station.Models;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,6 +14,7 @@ namespace Cashier_Station
 {
     public partial class AdminAddTicketWindow : Form
     {
+        DataBase db;
         public AdminAddTicketWindow()
         {
             InitializeComponent();
@@ -26,7 +28,50 @@ namespace Cashier_Station
 
         private void SubmitButton_Click(object sender, EventArgs e)
         {
-            
+            var result = MessageBox.Show("Ви впевненні, що хочете додати цю інформацію?", "Додавання інформації", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                try
+                {
+
+
+                    db = new DataBase();
+                    if (IdTicketTextBox.Text == "" || PriceTicketTextBox.Text == "" || RouteNumberTextBox.Text == "")
+                    {
+                        MessageBox.Show("Поля не можуть бути пустими або від'ємними", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        db.OpenConnection();
+                        string query = "INSERT INTO ticket (id, RouteId, Price, IsActive) VAlUES (@id, @RouteId, @Price, @IsActive)";
+
+                        using (MySqlCommand cmd = new MySqlCommand(query, db.GetConnection()))
+                        {
+
+                            cmd.Parameters.AddWithValue("@id", int.Parse(IdTicketTextBox.Text));
+                            cmd.Parameters.AddWithValue("@RouteId", int.Parse(RouteNumberTextBox.Text));
+                            cmd.Parameters.AddWithValue("@Price", int.Parse(PriceTicketTextBox.Text));
+                            cmd.Parameters.AddWithValue("@IsActive", false);
+
+
+                            cmd.ExecuteNonQuery();
+                        }
+                        Console.WriteLine("Дані успішно додано");
+                        MessageBox.Show("Дані успішно додано", "Успіх", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("При додаванні даних у таблицю ticket виникла помилка");
+                    Console.WriteLine($"Помилка: {ex.Message}");
+                    MessageBox.Show("Дані не були додані до бази даних", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                finally
+                {
+                    db.CloseConnection();
+                }
+
+            }
 
         }
     }
